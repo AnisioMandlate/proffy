316ad732830d87ef07e08615ac7f8d94d4f5cbb8
Anisio Mandlate <anisiomandlate@gmail.com>
2020-08-07 16:07:41 +0200

    feat: Add the schedule block

--- src/components/TeacherForm/index.tsx
+++ src/components/TeacherForm/index.tsx
@@ -42,6 +42,29 @@ formulário de inscrição."
           />
           <Input name="cost" label="Custo da aula por hora" />
         </fieldset>
+        <fieldset>
+          <legend>
+            Horários disponíveis
+            <button type="button">+ Novo horário</button>
+          </legend>
+          <div className="schedule-item">
+            <Select
+              name="week_day"
+              label="Dia da semana"
+              options={[
+                { value: "0", label: "Domingo" },
+                { value: "1", label: "Segunda-feira" },
+                { value: "2", label: "Terça-feira" },
+                { value: "3", label: "Quarta-feira" },
+                { value: "4", label: "Quinta-feira" },
+                { value: "5", label: "Sexta-feira" },
+                { value: "6", label: "Sábado" },
+              ]}
+            />
+            <Input name="from" label="Das" type="time" />
+            <Input name="to" label="Até" type="time" />
+          </div>
+        </fieldset>
         <footer>
           <p>
             <img src={warnigIcon} alt="Aviso Importante" />
