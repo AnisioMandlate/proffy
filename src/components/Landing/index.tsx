7bda781d08f7d67ed63b17b989427c9e4cc56399
Anisio Mandlate <anisiomandlate@gmail.com>
2020-08-07 16:58:13 +0200

    feat: Add axios library and showed total of connections realized

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import logo from "../../assets/images/logo.svg";
 import landing from "../../assets/images/landing.svg";
@@ -6,8 +6,18 @@ import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 import { Link } from "react-router-dom";
+import api from "../../services";
 
 const Landing = () => {
+  const [totalConnection, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get("connections").then((response) => {
+      const { total } = response.data;
+      setTotalConnections(total);
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -28,7 +38,7 @@ const Landing = () => {
           </Link>
         </div>
         <span className="total-connections">
-          Total de 200 conexões já realizadas{" "}
+          Total de {totalConnection} conexões já realizadas{" "}
           <img src={purpleHeartIcon} alt="Purpe Heart" />
         </span>
       </div>
