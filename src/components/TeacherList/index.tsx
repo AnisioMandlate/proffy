92c9685da312ba1b3a1bdbae1ec5b5d3c2e8e6dc
Anisio Mandlate <anisiomandlate@gmail.com>
2020-08-07 19:50:54 +0200

    feat: Add button on teacher's list

--- src/components/TeacherList/index.tsx
+++ src/components/TeacherList/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
 import "./styles.css";
 import PageHeader from "../PageHeader";
 import TeacherItem from "../TeacherItem";
@@ -6,29 +6,41 @@ import Input from "../Input";
 import Select from "../Select";
 
 const TeacherList = () => {
+  const [subject, setSubject] = useState("");
+  const [week_day, setWeekDay] = useState("");
+  const [time, setTime] = useState("");
+
+  function searchTeachers(e: FormEvent) {
+    e.preventDefault();
+  }
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis">
-        <form id="search-teachers">
+        <form id="search-teachers" onSubmit={searchTeachers}>
           <Select
             name="subject"
             label="Matéria"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
             options={[
-              { value: "Artes", label: "Artes" },
-              { value: "Historia", label: "Historia" },
-              { value: "Matematica", label: "Matematica" },
               { value: "Desenho", label: "Desenho" },
               { value: "Filosofia", label: "Filosofia" },
-              { value: "Fisica", label: "Fisica" },
-              { value: "Ingles", label: "Ingles" },
-              { value: "Portugues", label: "Portugues" },
-              { value: "Frances", label: "Frances" },
+              { value: "Física", label: "Física" },
+              { value: "Francês", label: "Francês" },
               { value: "Geografia", label: "Geografia" },
+              { value: "História", label: "História" },
+              { value: "Inglês", label: "Inglês" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Português", label: "Português" },
+              { value: "Química", label: "Química" },
             ]}
           />
           <Select
             name="week_day"
             label="Dia da semana"
+            value={week_day}
+            onChange={(e) => setWeekDay(e.target.value)}
             options={[
               { value: "0", label: "Domingo" },
               { value: "1", label: "Segunda-feira" },
@@ -39,7 +51,13 @@ const TeacherList = () => {
               { value: "6", label: "Sábado" },
             ]}
           />
-          <Input name="time" label="Hora" />
+          <Input
+            name="time"
+            label="Hora"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+          />
+          <button type="submit">Procurar</button>
         </form>
       </PageHeader>
       <main>
