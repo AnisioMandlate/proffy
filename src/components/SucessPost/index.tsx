5bc172ce0bc11707288593a382a4403558525080
Anisio Mandlate <anisiomandlate@gmail.com>
2020-08-17 13:09:42 +0200

    feat: Add the image and text on saved page

--- src/components/SucessPost/index.tsx
+++ src/components/SucessPost/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./styles.css";
+import checkIcon from "../../assets/images/icons/success-check-icon.svg";
 
 const SucessPost = () => {
   return (
     <div id="sucess">
-      <h1>Hello</h1>
+      <img src={checkIcon} alt="Cadastro feito" />
+      <h1>Cadastro salvo!</h1>
+      <p>
+        Tudo certo, seu cadastro está na nossa lista de professores.
+        <br />
+        Agora é só ficar de olho no seu WhatsApp.
+      </p>
       <button>Ir a Home</button>
     </div>
   );
