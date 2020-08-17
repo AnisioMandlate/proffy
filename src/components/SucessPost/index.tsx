5ecbe99d2486b70ec7d3601eb3a91e448e49de5e
Anisio Mandlate <anisiomandlate@gmail.com>
2020-08-17 12:19:07 +0200

    feat: Add the saved page

---
+++ src/components/SucessPost/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SucessPost = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+export default SucessPost;
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Landing from "./components/Landing";
 import TeacherList from "./components/TeacherList";
 import TeacherForm from "./components/TeacherForm";
+import SucessPost from "./components/SucessPost";
 
 const Routes = () => {
   return (
@@ -10,6 +11,7 @@ const Routes = () => {
       <Route exact path="/" component={Landing} />
       <Route exact path="/study" component={TeacherList} />
       <Route exact path="/give-classes" component={TeacherForm} />
+      <Route exact path="/saved" component={SucessPost} />
     </Router>
   );
 };
